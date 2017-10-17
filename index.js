68e7aea51a94ba16563f130367932141249d91d8
Fabricio Francisco Mendes da Silva <famendes@br.ibm.com>
2017-10-17 12:20:28 -0200

    #5 - Node.js database tutorial

--- index.js
+++ index.js
@@ -58,6 +58,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -74,6 +75,128 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (request, response) => {
   response.render('home', {
     name: 'John'
+  });
+});
+
+// parse application/json
+app.use(bodyParser.json());
+
+// storing data in a global variable
+// app.get('/users', (request, response) => {
+//   response.json(users);
+// });
+
+// const users = [];
+
+// app.post('/users', function(req, res) {
+//   // retrive user posted data from the body
+//   const user = req.body;
+//   users.push({
+//     name: user.name,
+//     age: user.age
+//   });
+//   res.send('successfully registered');
+// });
+
+// storing data in a file
+// const fs = require('fs');
+
+// app.get('/users', (request, response) => {
+//   fs.readFile('users.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     response.json(JSON.parse(data));
+//   })
+// });
+
+// app.post('/users', function(req, res) {
+//   const user = req.body;
+//   fs.appendFile(
+//     'users.txt',
+//     JSON.stringify({
+//       name: user.name,
+//       age: user.age
+//     }),
+//     (err) => {
+//       res.send('successfully registered');
+//     }
+//   );
+// });
+
+// Node.js Database Interaction
+// CREATE TABLE users(name VARCHAR(20), age SMALLINT);
+const { Client } = require('pg');
+
+// client.connect((err) => {
+//   if (err) {
+//     return console.log('error fetching client from pool', err);
+//   }
+
+//   client.query('SELECT $1::varchar AS my_first_query', ['node_hero'], (err, result) => {
+
+//     if (err) {
+//       return console.log('error happened during query', err);
+//     }
+
+//     console.log(result.rows[0]);
+//     process.exit(0);
+//     // client.close();
+//   });
+// });
+
+app.get('/users', (request, response, next) => {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'node_hero',
+    user: 'fabriciosilva',
+    password: 'root'
+  });
+
+  client.connect((err) => {
+    if (err) {
+      // pass the error to the express error handler
+      return next(err);
+    }
+
+    client.query('SELECT name, age FROM users', [], (err, result) => {
+      if (err) {
+        // pass the error to the express error handler
+        return next(err);
+      }
+
+      client.end();
+      response.json(result.rows);
+    });
+  });
+
+});
+
+app.post('/users', (request, response, next) => {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'node_hero',
+    user: 'fabriciosilva',
+    password: 'root'
+  });
+
+  const user = request.body;
+
+  client.connect((err) => {
+    if (err) {
+      // pass the error to the express error handler
+      return next(err);
+    }
+
+    client.query('INSERT INTO users (name, age) VALUES ($1, $2)', [user.name, user.age], (err, result) => {
+      if (err) {
+        // pass the error to the express error handler
+        return next(err);
+      }
+
+      client.end();
+      response.send(200);
+    });
   });
 });
 
